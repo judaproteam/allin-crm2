2b6a3e8846854543faee9e531ba1736a97c57b21
juda <jud.zvi85@gmail.com>
2024-07-29 12:03:05 +0300

    numbox nan to 0

--- components/Numbox.tsx
+++ components/Numbox.tsx
@@ -10,7 +10,7 @@ export function Numbox({ title, num, info = false, className = '' }) {
           <span>{info}</span>
         </>
       )}
-      <h2>{toCurrency(num)}</h2>
+      <h2>{toCurrency(num | 0)}</h2>
     </main>
   )
 }
@@ -18,20 +18,20 @@ export function Numbox({ title, num, info = false, className = '' }) {
 export function Numboxs({ sales }) {
   return (
     <>
-      <Numbox title="סיכונים" num={sales['סיכונים'] | 0} />
-      <Numbox title="פיננסי - שוטף משונת" num={sales['פיננסי']['הפקדה חודשית'] | 0} />
-      <Numbox title="פיננסי - ניודים" num={sales['פיננסי']['ניוד'] | 0} />
-      <Numbox title="פיננסי - הפקדה חד פעמית" num={sales['פיננסי']['הפקדה חד פעמית'] | 0} />
+      <Numbox title="סיכונים" num={sales['סיכונים']} />
+      <Numbox title="פיננסי - שוטף משונת" num={sales['פיננסי']['הפקדה חודשית']} />
+      <Numbox title="פיננסי - ניודים" num={sales['פיננסי']['ניוד']} />
+      <Numbox title="פיננסי - הפקדה חד פעמית" num={sales['פיננסי']['הפקדה חד פעמית']} />
 
-      <Numbox title="פנסיוני - שוטף משונת" num={sales['פנסיוני']['הפקדה חודשית'] | 0} />
-      <Numbox title="פנסיוני - ניודים" num={sales['פנסיוני']['ניוד'] | 0} />
+      <Numbox title="פנסיוני - שוטף משונת" num={sales['פנסיוני']['הפקדה חודשית']} />
+      <Numbox title="פנסיוני - ניודים" num={sales['פנסיוני']['ניוד']} />
 
-      <Numbox title="אלמנטרי" num={sales['אלמנטרי'] | 0} />
-      <Numbox title="כתב שירות חיצוני" num={sales['כתב שירות חיצוני'] | 0} />
-      <Numbox title="נסיעות לחול" num={sales['נסיעות לחול'] | 0} />
-      <Numbox title="קצבה מיידית" num={sales['קצבה מיידית'] | 0} />
-      <Numbox title="תאונות אישיות" num={sales['תאונות אישיות'] | 0} />
-      <Numbox title='אכ"ע' num={sales['אכ"ע'] | 0} />
+      <Numbox title="אלמנטרי" num={sales['אלמנטרי']} />
+      <Numbox title="כתב שירות חיצוני" num={sales['כתב שירות חיצוני']} />
+      <Numbox title="נסיעות לחול" num={sales['נסיעות לחול']} />
+      <Numbox title="קצבה מיידית" num={sales['קצבה מיידית']} />
+      <Numbox title="תאונות אישיות" num={sales['תאונות אישיות']} />
+      <Numbox title='אכ"ע' num={sales['אכ"ע']} />
     </>
   )
 }
