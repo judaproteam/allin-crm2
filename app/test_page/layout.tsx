2179ad65b1ef99fb4b39da20016335d2c2ee602b
juda <jud.zvi85@gmail.com>
2024-08-09 02:01:45 +0300

    delete test page

--- app/test_page/layout.tsx
+++
@@ -1,5 +0,0 @@
-export default function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
-  console.log('test layout')
-
-  return <div className="bg-cyan-100 w-full h-screen">{children}</div>
-}
